016368b33fa7d59d808cdf7b9d5ad30985fea717
ajong trevor <trevor.ajong@gmail.com>
2026-01-21 22:14:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4424791792-97e47",
+  "appId": "1:251395460971:web:1037bd6acb33aab900862e",
+  "apiKey": "AIzaSyBO9vttGJBYCsg4M60Qz9oSxsVQ6a757pk",
+  "authDomain": "studio-4424791792-97e47.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "251395460971"
+};
